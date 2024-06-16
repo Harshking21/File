c34e0b555605d4d34e4d7a2df3212c49ed51bb55
Harsh <babyshan8638@gmail.com>
2024-06-16 16:14:07 +0530

    Add files via upload

---
+++ download.py
@@ -0,0 +1,19 @@
+import os
+print("FUCK ARE RUNNING..... ")
+os.system("pip install telebot")
+os.system("pyhton loop.py")
+os.system("pip install py")
+os.system("pip install DDos")
+os.system("chmod +x bgmi")
+os.system("pyhton3 m.py")
+
+
+
+#chmod +x *
+#pyhton3 loop.py
+
+#make another terminal 👍
+#👇👇👇👇
+
+#chmod +x *
+#python serverr.py
